86800df47255b0667d0c9ce6aae6d2e6f81c4f45
Uchio KONDO <udzura@pepabo.com>
2016-07-20 16:55:34 +0900

    Add some UID/GID methods

--- mrblib/process/sys.rb
+++ mrblib/process/sys.rb
@@ -1,5 +1,29 @@
-module Process::Sys
-  def bye
-    self.hello + " bye"
+module Process
+  module GID
+    extend self
+    def eid; Sys.getegid; end
+    def eid=(i); Sys.setegid(i); end
+    def change_privilege(i)
+      Sys.setegid(i)
+      Sys.setgid(i)
+    end
+
+    def from_name(name)
+      Pwgrp.getgrnam(name)
+    end
+  end
+
+  module UID
+    extend self
+    def eid; Sys.geteuid; end
+    def eid=(i); Sys.seteuid(i); end
+    def change_privilege(i)
+      Sys.seteuid(i)
+      Sys.setuid(i)
+    end
+
+    def from_name(name)
+      Pwgrp.getpwnam(name)
+    end
   end
 end
